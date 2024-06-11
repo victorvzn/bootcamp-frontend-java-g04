19b8609699f50fad334db3f69d2699677c06d05d
Victor Villazón <victorzn23@gmail.com>
2024-06-10 19:39:00 -0500

    semana02, dia01, javascript basico

---
+++ semana02/dia01-javascript-basico/main.js
@@ -0,0 +1,77 @@
+// COMENTARIOS
+
+// console.log('Hola Javascript! 3')
+
+/*
+  Esto
+  es
+  un
+  comentario
+  de
+  múltiples
+  líneas
+*/
+
+// TIPOS DE DATOS (https://lenguajejs.com/javascript/)
+
+// PRIMITIVOS: Number, String, Boolean (true, false), undefined, null, etc
+
+// NUMBER
+
+const numero1 = 20
+const numero2 = 20.50
+const numero3 = -36
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+
+console.log(typeof numero1)
+console.log(typeof numero2)
+console.log(typeof numero3)
+
+// STRING O Cadena de texto
+
+const alumna = 'Claudia'
+const alumno = "Diego"
+const numero4 = "200"
+const numero5 = 200
+
+console.log(alumna)
+console.log(alumno)
+console.log(numero4)
+console.log(numero5)
+
+console.log(typeof alumna)
+console.log(typeof alumno)
+console.log(typeof numero4)
+console.log(typeof numero5)
+
+// BOOLEAN -> false o true
+
+const tieneDescuento = true
+
+console.log(tieneDescuento)
+console.log(typeof tieneDescuento)
+
+// UNDEFINED
+
+let nombre
+
+console.log(nombre)
+console.log(typeof nombre)
+
+// NULL
+
+// let apellido0 = undefined // ❌
+let apellido = null
+
+console.log(apellido)
+console.log(typeof apellido)
+
+// Object
+// Function
+// BigInt
+// Symbol
+
+// VARIABLES Y CONSTANTES
