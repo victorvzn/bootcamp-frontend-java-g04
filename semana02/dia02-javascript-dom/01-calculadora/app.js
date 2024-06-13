e27fb66df586f983a0c2e7f7cb7780d49ae28fdf
Victor Villazón <victorzn23@gmail.com>
2024-06-12 22:35:46 -0500

    semana02, dia02, javascript dom, calculadora

--- semana02/dia02-javascript-dom/01-calculadora/app.js
+++ semana02/dia02-javascript-dom/01-calculadora/app.js
@@ -1,1 +1,15 @@
 console.log('Hola JS!')
+
+
+const inputDisplay = document.querySelector('#inputDisplay') // Consulta solo un elemento a la vez
+const buttons = document.querySelectorAll('button')
+
+console.log(inputDisplay)
+console.log(buttons)
+
+// const titleH1 = document.getElementById('title')
+const titleH1 = document.querySelector('#title')
+// const titleH1 = document.querySelector('.mytitle')
+// const titleH1 = document.querySelector('h1')
+console.log(titleH1.textContent)
+titleH1.textContent = 'Calculadora v1.1'
