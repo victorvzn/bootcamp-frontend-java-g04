1e8a0bb1082f8293e46d340050e591a18ab4546c
Victor Villazón <victorzn23@gmail.com>
2024-06-14 23:06:12 -0500

    semana02, dia03, javascript countries

--- semana02/dia03-javascript-countries/modules/main.js
+++ semana02/dia03-javascript-countries/modules/main.js
@@ -1,11 +1,34 @@
 import fetchCountries from './services.js'
 import { renderCountries, suma } from './utils.js'
 
+let countryData = []
+
+const searchInput = document.querySelector('.app__input')
+
+searchInput.addEventListener('input', function (event) {
+  const { value } = event.target
+  const loweredValue = value.toLowerCase()
+
+  const filteredCountries = countryData.filter(country => {
+    const loweredName = country.name.common.toLowerCase()
+
+    // TODO: RETO 01 - Adicionalmente necesitamos filtrar los paises por su capital
+
+    return loweredName.includes(loweredValue)
+  })
+
+  renderCountries(filteredCountries)
+})
+
+// TODO: RETO 02 - Filtrar los paises por su region usando el select. Usen el evento input también.
+
 fetchCountries()
   .then(countries => {
     console.log(countries)
 
+    countryData = countries
+
     renderCountries(countries)
 
-    console.log(suma(9, 8))
+    // console.log(suma(9, 8))
   })
--- semana02/dia03-javascript-countries/modules/utils.js
+++ semana02/dia03-javascript-countries/modules/utils.js
@@ -1,5 +1,35 @@
 export const renderCountries = (countries = []) => {
-  console.log('Estoy en la función renderCountries', countries)
+  // console.log('Estoy en la función renderCountries', countries)
+
+  // TODO: RETO03 - Formatear el campo population usando separadores de miles.
+  // Ayuda: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+
+  const countryListElement = document.querySelector('.app__list')
+
+  let countryList = ''
+
+  countries.forEach(country => {
+    countryList += `
+      <article class="country">
+        <img class="country__flag" src="${country.flags.png}" />
+
+        <div class="country__wrapper">
+          <h2>${country.name.common}</h2>
+          <div class="country__description">
+            <strong>Population:</strong> ${country.population}
+          </div>
+          <div class="country__description">
+            <strong>Region:</strong> ${country.region}
+          </div>
+          <div class="country__description">
+            <strong>Capital:</strong> ${country.capital.join(', ')}
+          </div>
+        </div>
+      </article>
+    `
+  })
+
+  countryListElement.innerHTML = countryList
 }
 
 export const suma = (num1, num2) => {
