a161e73cdea416686cdaf9d95951227fde360849
Victor Villazón <victorzn23@gmail.com>
2024-06-14 19:55:36 -0500

    semana02, dia03, javascript dom, todo app

--- semana02/dia02-javascript-dom/02-todo-app/app.js
+++ semana02/dia02-javascript-dom/02-todo-app/app.js
@@ -1,1 +1,38 @@
 console.log('Hola JS')
+
+const taskInput = document.getElementById('taskInput')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+// console.log(taskInput)
+// console.log({ taskAdd })
+// console.log({ taskList })
+
+// EVENTOS
+
+// ELEMENTO.addEventListener(NOMBRE_EVENTO, FUNCION)
+
+taskAdd.addEventListener('click', function (event) {
+  // Se ejecutará cuando hagamos click en el botón 'Añadir tarea'
+  // console.log('CLick!!!')
+  // console.log(taskInput.value)
+
+  // Forma de crear elementos dinámicamente con JS
+  // const button = document.createElement('button')
+  // button.textContent = 'Hola soy un botón'
+  // document.body.appendChild(button)
+
+  // TODO: Validar la entrada del usuario
+
+  // Añadiendo un elemento li al elemento con la clase task__list
+  const li = document.createElement('li')
+
+  const span = document.createElement('span')
+  span.textContent = taskInput.value
+  li.appendChild(span)
+
+  taskList.appendChild(li)
+
+  // TODO: Limpiar la caja de texto después de crear la tarea
+
+})
