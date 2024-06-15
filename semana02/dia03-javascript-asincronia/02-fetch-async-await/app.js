54ab079f296ed12ca4f0eb8cfa8cd24819ffefcb
Victor Villazón <victorzn23@gmail.com>
2024-06-14 21:13:31 -0500

    semana02, dia03, javascript asincronia, fetch + async/await

---
+++ semana02/dia03-javascript-asincronia/02-fetch-async-await/app.js
@@ -0,0 +1,71 @@
+console.log('Hola JS')
+
+const url = 'https://jsonplaceholder.typicode.com/postsxxxxx'
+
+// METHODS: GET, POST, PUT DELETE, PATCH
+
+// async function fetchPosts() {}
+// const fetchPosts = async () => {}
+
+const fetchPosts = async () => {
+  // return fetch(url)
+  //   .then(response => response.json())
+
+  try {
+    const response = await fetch(url)
+
+    console.log(response)
+
+    // OK === STATUS CODE 200 / 400
+    if (!response.ok) {
+      throw new Error('ERROR HTTP:', response.status)
+    }
+
+    const json = await response.json()
+
+    return json // Aquí podemos procesar o modificar el json entrante
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+const renderPosts = (posts = []) => {
+  const appDiv = document.getElementById('app')
+
+  let postList = ''
+
+  posts.forEach(post => {
+    // console.log(post)
+
+    postList += `
+      <div>
+        <h2>${post.id} - ${post.title}</h2>
+        <p>${post.body}</p>
+      </div>
+    `
+  })
+
+  // console.log(postList)
+
+  // appDiv.innerHTML = '<h1>Uso de promesas</h1>'
+  appDiv.innerHTML = postList
+}
+
+async function init() {
+  const posts = await fetchPosts()
+
+  renderPosts(posts)
+}
+
+init()
+
+// const res = fetchPosts()
+// res.then(data => renderPosts(data))
+
+// fetchPosts()
+//   .then(posts => {
+//     console.log(posts)
+//     renderPosts(posts)
+//   })
