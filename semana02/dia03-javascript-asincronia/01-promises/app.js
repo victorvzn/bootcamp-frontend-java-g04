37d4d6f2e1f6859089f0e90ed96408e572919c7e
Victor Villazón <victorzn23@gmail.com>
2024-06-14 20:52:45 -0500

    semana02, dia03, javascript asincronia, fetch + promises

--- semana02/dia03-javascript-asincronia/01-promises/app.js
+++ semana02/dia03-javascript-asincronia/01-promises/app.js
@@ -1,1 +1,40 @@
 console.log('Hola JS')
+
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+// METHODS: GET, POST, PUT DELETE, PATCH
+
+function fetchPosts() {
+  console.log('Llamando al api de JSONPLACEHOLDER')
+
+  return fetch(url)
+    .then(response => response.json())
+}
+
+const renderPosts = (posts = []) => {
+  const appDiv = document.getElementById('app')
+
+  let postList = ''
+
+  posts.forEach(post => {
+    // console.log(post)
+
+    postList += `
+      <div>
+        <h2>${post.id} - ${post.title}</h2>
+        <p>${post.body}</p>
+      </div>
+    `
+  })
+
+  // console.log(postList)
+
+  // appDiv.innerHTML = '<h1>Uso de promesas</h1>'
+  appDiv.innerHTML = postList
+}
+
+fetchPosts()
+  .then(posts => {
+    console.log(posts)
+    renderPosts(posts)
+  })
