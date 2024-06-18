e0047596908d432f6e6f9da2e5a91533baa26bd8
Victor Villazón <victorzn23@gmail.com>
2024-06-17 22:51:00 -0500

    semana03, dia01, react imc app

--- semana03/dia01-react-imc-app/src/App.jsx
+++ semana03/dia01-react-imc-app/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react"
 
+const formatNumber = (number) => {
+  const localeDefault = 'en-US'
+
+  const options = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }
+
+  return new Intl.NumberFormat(localeDefault, options).format(number)
+}
+
 export default function App() {
   // const weight = 12
   // useState nos ayuda a generar un estado para manipular dentro de nuestro componente
 
   const [weight, setWeight] = useState(0) // useState Devuelve una arreglo con una variable de lectura y una función de escritura (setter)
+  const [height, setHeight] = useState(0)
 
   const handleWeight = (event) => {
     console.log(event.target.value)
@@ -13,8 +25,39 @@ export default function App() {
     setWeight(value)
   }
 
+  const handleHeight = (event) => {
+    const value = event.target.value
+
+    setHeight(value)
+  }
+
+  const imc = formatNumber(weight / ((height / 100) ** 2))
+
+  // * Baja: < 18.5
+  // * Normal: < 18.5 - 24.9
+  // * Sobrepeso: < 25 - 29.9 
+  // * Obeso: > 30
+
+  const bodyMassIndexResult = () => {
+    let result = ''
+
+    if (imc < 18.5) {
+      result = 'Baja'
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      result = 'Normal'
+    } else if (imc >= 25 && imc <= 29.9) {
+      result = 'Sobrepeso'
+    } else if (imc > 30) {
+      result = 'Obeso'
+    } else {
+      result = 'Datos incorrectos'
+    }
+
+    return result
+  }
+
   return (
-    <section className="w-[300px] mx-auto bg-slate-200 p-4">
+    <section className="w-[400px] mx-auto bg-slate-200 p-4">
       <h1 className="text-3xl text-red-600 font-bold text-center">IMC Calculator</h1>
 
       <div className="pt-4">
@@ -28,17 +71,18 @@ export default function App() {
       </div>
 
       <div>
-        <p>Height: 0 cm</p>
+        <p>Height: {height} cm</p>
         <input
           type="range"
           min="50"
           max="200"
+          onChange={handleHeight}
         />
       </div>
 
-      <p className="font-bold ">Tu IMC es 0.0</p>
+      <p className="font-bold ">Tu IMC es {imc}</p>
 
-      <p className="font-bold text-2xl">Estado de IMC: ???</p>
+      <p className="font-bold text-2xl">Estado de IMC: {bodyMassIndexResult()}</p>
     </section>
   )
 }
