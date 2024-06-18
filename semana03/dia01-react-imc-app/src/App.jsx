1128f694bab5f3c5d61cc7effacdad90efb717a0
Victor Villazón <victorzn23@gmail.com>
2024-06-17 22:27:59 -0500

    semana03, dia01, react imc app

--- semana03/dia01-react-imc-app/src/App.jsx
+++ semana03/dia01-react-imc-app/src/App.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react"
+
 export default function App() {
+  // const weight = 12
+  // useState nos ayuda a generar un estado para manipular dentro de nuestro componente
+
+  const [weight, setWeight] = useState(0) // useState Devuelve una arreglo con una variable de lectura y una función de escritura (setter)
+
+  const handleWeight = (event) => {
+    console.log(event.target.value)
+    const value = event.target.value
+
+    setWeight(value)
+  }
+
   return (
     <section className="w-[300px] mx-auto bg-slate-200 p-4">
       <h1 className="text-3xl text-red-600 font-bold text-center">IMC Calculator</h1>
 
       <div className="pt-4">
-        <p>Weight: 0 kg</p>
+        <p>Weight: {weight} kg</p>
         <input
           type="range"
           min="50"
           max="200"
+          onChange={handleWeight}
         />
       </div>
 
