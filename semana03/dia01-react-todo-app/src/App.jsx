7027b58d46b4e0a4d1fa77631fb3ee3d344c49e8
Victor Villazón <victorzn23@gmail.com>
2024-06-19 19:38:53 -0500

    semana03, dia02, react todo app

--- semana03/dia01-react-todo-app/src/App.jsx
+++ semana03/dia01-react-todo-app/src/App.jsx
@@ -1,9 +1,61 @@
+import { useState } from "react"
+
 const App = () => {
+  const DEFAULT_TODOS = [
+    {
+      id: 1,
+      title: 'Aprender JS',
+      completed: true
+    },
+    {
+      id: 2,
+      title: 'Aprender CSS',
+      completed: true
+    },
+    {
+      id: 2,
+      title: 'Aprender React.js + Tailwindcss',
+      completed: false
+    }
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
+
   return (
     <main>
       <h1 className="text-2xl font-bold text-center">
         TODO APP
       </h1>
+
+      {/* {JSON.stringify(todos)} */}
+
+      <form>
+        <input
+          className="w-full border my-3 p-3"
+          type="text"
+          placeholder="¿Qué deseas hacer hoy?"
+          required
+        />
+      </form>
+
+      <div>
+        <span>2 de 3 completadas</span>
+        <button>
+          Limpir tareas completadas
+        </button>
+      </div>
+
+      <section>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            <div>
+              <span>title 1</span>
+              <button>❌</button>
+            </div>
+          </li>
+        </ul>
+      </section>
     </main>
   )
 }
