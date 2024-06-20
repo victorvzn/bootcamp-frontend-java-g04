3cb7ab14bc945e4b8a617c30bbeb3f3ad279e95b
Victor Villazón <victorzn23@gmail.com>
2024-06-19 20:11:11 -0500

    semana03, dia02, react todo app

--- semana03/dia01-react-todo-app/src/App.jsx
+++ semana03/dia01-react-todo-app/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       completed: true
     },
     {
-      id: 2,
+      id: 3,
       title: 'Aprender React.js + Tailwindcss',
       completed: false
     }
@@ -22,12 +22,14 @@ const App = () => {
   const [todos, setTodos] = useState(DEFAULT_TODOS)
 
   return (
-    <main>
-      <h1 className="text-2xl font-bold text-center">
-        TODO APP
-      </h1>
+    <main
+      className="bg-yellow-100 w-full max-w-sm mx-auto mt-10 border border-yellow-600 rounded-lg shadow-lg p-4"
+    >
+      <h1 className="text-2xl font-bold text-center">TODO APP</h1>
 
       {/* {JSON.stringify(todos)} */}
+
+      {/* {['victor', 'paul']} */}
 
       <form>
         <input
@@ -38,22 +40,38 @@ const App = () => {
         />
       </form>
 
-      <div>
-        <span>2 de 3 completadas</span>
-        <button>
-          Limpir tareas completadas
+      <section className="flex justify-between items-center">
+        <span className="font-bold">
+          2 de 3
+        </span>
+        <button
+          className="bg-blue-500 rounded-lg px-2 py-1 text-white hover:bg-blue-700 duration-300"
+        >
+          Limpiar completadas
         </button>
-      </div>
+      </section>
 
-      <section>
-        <ul>
-          <li>
-            <input type="checkbox" />
-            <div>
-              <span>title 1</span>
-              <button>❌</button>
-            </div>
-          </li>
+      <section className="mt-8">
+        <ul className="flex flex-col gap-2">
+          {
+            todos.map(
+              function(todo) {
+                return (
+                  <li className="flex" key={todo.id}>
+                    <input type="checkbox" className="mr-2" />
+                    <div className="w-full flex justify-between items-center">
+                      <span>{todo.title}</span>
+                      <button
+                        className="bg-red-300 rounded-lg px-1 py-1"
+                      >
+                        ❌
+                      </button>
+                    </div>
+                  </li>
+                )
+              }
+            )
+          }
         </ul>
       </section>
     </main>
