e643ad45fc6892294b257eef56120104e3e7cd3e
Victor Villazón <victorzn23@gmail.com>
2024-06-24 20:30:01 -0500

    semana04, dia01, react appointments app

--- semana04/dia03-react-appointments-app/src/components/appointments/AppointmentsForm.jsx
+++ semana04/dia03-react-appointments-app/src/components/appointments/AppointmentsForm.jsx
@@ -13,48 +13,70 @@ const AppointmentsForm = () => {
 
   const [form, setForm] = useState(INITIAL_FORM_STATE)
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log('Guardando ...')
+  }
+
+  const handleChange = (event) => {
+    // console.log({ input: event.target })
+    const { name, value } = event.target
+
+    setForm({ ...form, [name]: value })
+  }
+
   return (
-    <section className="w-96 p-4 border border-red-300 ">
+    <section className="w-96 p-4 border rounded-md">
       <h2 className="text-2xl text-center mb-4">Nuevo paciente</h2>
+
+      <pre>{JSON.stringify(form, null, 2)}</pre>
 
       <form
         className="flex flex-col gap-4"
+        onSubmit={handleSubmit}
       >
         <input
           type="text"
           name="petName"
           placeholder="Nombre de la mascota"
           className="border p-3 shadow-md rounded-md w-full"
+          onChange={handleChange}
         />
         <input
           type="number"
           name="petAge"
           placeholder="Edad de la mascota"
           className="border p-3 shadow-md rounded-md w-full"
+          onChange={handleChange}
         />
         <input
           type="text"
           name="ownerName"
           placeholder="Dueño de la mascota"
           className="border p-3 shadow-md rounded-md w-full"
+          onChange={handleChange}
         />
         <input
           type="date"
           name="appointmentDate"
           placeholder="Fecha de la cita"
           className="border p-3 shadow-md rounded-md w-full"
+          onChange={handleChange}
         />
         <input
           type="time"
           name="appointmentTime"
           placeholder="Hora de la cita"
           className="border p-3 shadow-md rounded-md w-full"
+          onChange={handleChange}
         />
         <textarea
           name="symptoms"
           placeholder="Síntomas"
           rows="5"
           className="border p-3 shadow-md rounded-md w-full"
+          onChange={handleChange}
         >
         </textarea>
         <input
