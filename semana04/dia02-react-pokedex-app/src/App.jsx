89b794c668e73c1c5d318b426563bb13cb66c523
Victor Villazón <victorzn23@gmail.com>
2024-06-26 21:39:15 -0500

    semana04, dia02, react pokedex app

--- semana04/dia02-react-pokedex-app/src/App.jsx
+++ semana04/dia02-react-pokedex-app/src/App.jsx
@@ -7,11 +7,12 @@ const POKEAPI_URL = 'https://pokeapi.co/api/v2/pokemon'
 const App = () => {
   const [pokemons, setPokemons] = useState([])
   const [page, setPage] = useState(1)
+  const [total, setTotal] = useState(0)
+  const [limit] = useState(10)
 
-  const fetchPokemons = async (page = 1) => {
+  const fetchPokemons = async (page = 1, limit=10) => {
 
     // https://pokeapi.co/api/v2/pokemon?offset=10&limit=10
-    const limit = 10
     const offset = (page - 1) * limit
 
     const response = await fetch(`${POKEAPI_URL}?offset=${offset}&limit=${limit}`)
@@ -19,6 +20,8 @@ const App = () => {
     const data = await response.json()
   
     setPokemons(data.results)
+
+    setTotal(data.count)
   }
 
   useEffect(() => {
@@ -35,12 +38,20 @@ const App = () => {
 
   const handlePrev = () => {
     // TODO: Validar que la página sea mayor de cero
+    if (page <= 1) {
+      return
+    }
 
     setPage(page - 1)
   }
+
+  const lastPage = Math.ceil(total / limit)
   
   const handleNext = () => {
     // TODO: Validar que la página sea menor que la última página (131)
+    if (page >= lastPage) {
+      return
+    }
 
     setPage(page + 1)
   }
@@ -75,7 +86,7 @@ const App = () => {
         onClick={() => handlePrev()}>
           Previous
         </button>
-        <span className="flex justify-center items-center p-2">{page}</span>
+        <span className="flex justify-center items-center p-2">{page} of {lastPage}</span>
         <button className="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
         onClick={() => handleNext()}>
           Next
