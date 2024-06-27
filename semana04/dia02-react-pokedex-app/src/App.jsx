b494666913c88ab32d7fc9613885e050344064e5
Victor Villazón <victorzn23@gmail.com>
2024-06-26 20:21:10 -0500

    semana04, dia02, react pokedex app

--- semana04/dia02-react-pokedex-app/src/App.jsx
+++ semana04/dia02-react-pokedex-app/src/App.jsx
@@ -34,13 +34,16 @@ const App = () => {
       {/* <pre>{JSON.stringify(pokemons, null, 2)}</pre> */}
 
       {pokemons.map(pokemon => {
+        const id = pokemon.url.split('/').at(-2)
+        const pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
         return (
           <article
             className="border rounded-lg bg-slate-100 m-2"
             key={pokemon.url}
           >
             <h3 className="font-bold text-center uppercase p-3">{pokemon.name}</h3>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+            <img src={pokemonImage} />
           </article>
         )
       })}
