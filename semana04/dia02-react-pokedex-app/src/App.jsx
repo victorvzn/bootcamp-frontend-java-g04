464bebd3e25a42c7d4257ae1bdf364875294e7cb
Victor Villazón <victorzn23@gmail.com>
2024-06-26 20:55:41 -0500

    semana04, dia02, react pokedex app

--- semana04/dia02-react-pokedex-app/src/App.jsx
+++ semana04/dia02-react-pokedex-app/src/App.jsx
@@ -6,9 +6,15 @@ const POKEAPI_URL = 'https://pokeapi.co/api/v2/pokemon'
 
 const App = () => {
   const [pokemons, setPokemons] = useState([])
+  const [page, setPage] = useState(1)
 
-  const fetchPokemons = async () => {
-    const response = await fetch(POKEAPI_URL)
+  const fetchPokemons = async (page = 1) => {
+
+    // https://pokeapi.co/api/v2/pokemon?offset=10&limit=10
+    const limit = 10
+    const offset = (page - 1) * limit
+
+    const response = await fetch(`${POKEAPI_URL}?offset=${offset}&limit=${limit}`)
   
     const data = await response.json()
   
@@ -18,17 +24,27 @@ const App = () => {
   useEffect(() => {
     console.log('Solo me imprimo la primera vez')
 
-    fetchPokemons()
+    fetchPokemons(page)
+
+    console.log(page)
 
     // fetch(POKEAPI_URL)
     //   .then(response => response.json())
     //   .then(pokemons => setPokemons(pokemons.results))
-  }, [])
+  }, [page])
+
+  const handlePrev = () => {
+    setPage(page - 1)
+  }
+  
+  const handleNext = () => {
+    setPage(page + 1)
+  }
 
   return (
     <main className="container m-auto border p-4">
       <h1 className="text-3xl font-bold underline text-center">
-        Poké Catalog
+        Poké Catalog 1
       </h1>
 
       {/* <pre>{JSON.stringify(pokemons, null, 2)}</pre> */}
@@ -40,24 +56,26 @@ const App = () => {
 
           return (
             <article
-              className="border rounded-lg bg-slate-100 m-2"
+              className="border rounded-lg bg-slate-100 m-2 flex flex-col items-center"
               key={pokemon.url}
             >
-              <h3 className="font-bold text-center uppercase p-3">{pokemon.name}</h3>
-              <img src={pokemonImage} className="w-40" />
+              <h3 className="font-bold text-center uppercase p-3"> #{id} - {pokemon.name}</h3>
+              <img src={pokemonImage} width="160px" height="160px" />
             </article>
           )
         })}
       </div>
 
       <div className="flex justify-center">
-        <a href="#" className="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <button className="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+        onClick={() => handlePrev()}>
           Previous
-        </a>
-
-        <a href="#" className="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        </button>
+        <span className="flex justify-center items-center p-2">{page}</span>
+        <button className="flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+        onClick={() => handleNext()}>
           Next
-        </a>
+        </button>
       </div>
     </main>
   )
