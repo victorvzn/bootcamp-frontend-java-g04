4bc5b3d26db05f5455dc78fee24ef2778aba4dad
Victor Villazón <victorzn23@gmail.com>
2024-06-28 21:03:32 -0500

    semana04, dia03, react zustand intro shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/ProductHeader.jsx
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/ProductHeader.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { TbShoppingBag } from "react-icons/tb";
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const showCart = open ? 'translate-none' : 'translate-x-full'
+
+  const toggleSidebar = () => setOpen(!open)
+
   const classSidebar = 'fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-slate-100 w-80 text-black'
 
   return (
@@ -8,19 +15,19 @@ const Header = () => {
       <div className="container mx-auto flex justify-between">
         <h1 className="font-bold">Shopping Cart</h1>
       
-        <button className="text-3xl relative">
+        <button className="text-3xl relative" onClick={toggleSidebar}>
           <TbShoppingBag />
         </button>
 
         <div
-          className={`${classSidebar} translate-x-full`}
+          className={`${classSidebar} ${showCart}`}
         >
           <div className="flex justify-between">
             <h5 className="flex text-2xl font-semibold text-gray-500">
               Cart
             </h5>
 
-            <button>❌</button>
+            <button onClick={toggleSidebar}>❌</button>
           </div>
 
           <div className="flex flex-col gap-1 m-4">
