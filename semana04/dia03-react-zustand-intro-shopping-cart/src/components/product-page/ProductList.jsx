756f263b96e99fb7e756570643eee58b2ccf9bfb
Victor Villazón <victorzn23@gmail.com>
2024-06-28 20:28:10 -0500

    semana04, dia03, react zustand intro shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/ProductList.jsx
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/ProductList.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react"
+
+import axios from 'axios'
+
+// const fetchProductsWithAPIFetch = async () => {
+//   const url = 'https://dummyjson.com/products'
+
+//   const response = await fetch(url)
+
+//   return (await response).json()
+// }
+
+const fetchProductsWithAxios = async () => {
+  const url = 'https://dummyjson.com/products'
+
+  const response = await axios.get(url)
+
+  return response.data
+}
+
 const ProductList = () => {
+  const [productList, setProductList] = useState([])
+
+  useEffect(() => {
+    // Se ejecuta este useEffect la primera vez
+
+    // fetchProductsWithAPIFetch()
+    fetchProductsWithAxios()
+      .then(data => setProductList(data.products))
+  }, [])
+
   return (
-    <div>ProductList</div>
+    <section className="grid grid-cols-2 md:grid-cols-4 gap-8">
+      {/* TODO: Listar los nombres de los productos desde el estado productList */}
+
+      {productList.map(product => (
+        <p key={product.id}>{product.title}</p>
+      ))}
+
+      {/* <pre>{JSON.stringify(productList, null, 2)}</pre> */}
+    </section>
   )
 }
 
