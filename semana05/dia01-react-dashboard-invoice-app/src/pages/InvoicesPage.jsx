0afcb6e864c8227edaa1b25ae499c936c30054c1
Victor Villazón <victorzn23@gmail.com>
2024-07-01 20:02:16 -0500

    semana05, dia01, react dashboard invoice app

--- semana05/dia01-react-dashboard-invoice-app/src/pages/InvoicesPage.jsx
+++ semana05/dia01-react-dashboard-invoice-app/src/pages/InvoicesPage.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react"
+
 import InvoiceHeader from "../components/invoices-page/InvoiceHeader"
 import InvoiceList from "../components/invoices-page/InvoiceList"
+import { fetchInvoices } from "../services/invoices"
 
 const InvoicesPage = () => {
   const [invoices, setInvoices] = useState([])
+
+  useEffect(() => {
+    fetchInvoices()
+      .then((data) => setInvoices(data))
+  }, [])
 
   return (
     <>
       <InvoiceHeader />
 
       <InvoiceList />
+
+      <pre>{JSON.stringify(invoices, null, 2)}</pre>
     </>
   )
 }
