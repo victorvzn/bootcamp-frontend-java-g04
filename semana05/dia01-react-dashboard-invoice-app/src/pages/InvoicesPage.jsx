add527e8e6350c52ffa07578053d60b1c465b18a
Victor Villazón <victorzn23@gmail.com>
2024-07-01 19:58:31 -0500

    semana05, dia01, react dashboard invoice app

--- semana05/dia01-react-dashboard-invoice-app/src/pages/InvoicesPage.jsx
+++ semana05/dia01-react-dashboard-invoice-app/src/pages/InvoicesPage.jsx
@@ -1,10 +1,6 @@
 import InvoiceHeader from "../components/invoices-page/InvoiceHeader"
 import InvoiceList from "../components/invoices-page/InvoiceList"
 
-
-
-// https://668348284102471fa4c9a3dc.mockapi.io/api/v1/invoices
-
 const InvoicesPage = () => {
   const [invoices, setInvoices] = useState([])
 
--- semana05/dia01-react-dashboard-invoice-app/src/services/invoices.js
+++ semana05/dia01-react-dashboard-invoice-app/src/services/invoices.js
@@ -1,9 +1,21 @@
 const API_URL = 'https://668348284102471fa4c9a3dc.mockapi.io/api/v1'
 
 export const fetchInvoices = async () => {
-  const url = `${API_URL}/invoices`
+  try {
+    const url = `${API_URL}/invoices`
 
-  const response = await fetch(url)
+    const response = await fetch(url)
 
-  return response.json()
+    if (response.status === 400) {
+      throw new Error('4XX:', response.status)
+    }
+
+    if (!response.ok) {
+      throw new Error('ERROR:', response.status)
+    }
+
+    return response.json()
+  } catch(error) {
+    console.log(error)
+  }
 }
