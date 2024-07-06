68b8b3da95dbe7dd13a4716d95d1b7ae9e4f0000
Victor Villazón <victorzn23@gmail.com>
2024-07-05 21:33:27 -0500

    semana05, dia03, nextjs crud auth

--- semana05/dia03-nextjs-crud-auth/app/(home)/page.js
+++ semana05/dia03-nextjs-crud-auth/app/(home)/page.js
@@ -1,6 +1,23 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <section className="bg-white">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+          Next.js: CRUD + Auth
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
+          by <a href="https://victorvillazon.com">Victor Villazón</a>
+        </p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+          <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 ">
+            Sign In
+          </a>
+          <a href="#" className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+            Sign Up
+          </a>  
+        </div>
+      </div>
+  </section>
   )
 }
 
