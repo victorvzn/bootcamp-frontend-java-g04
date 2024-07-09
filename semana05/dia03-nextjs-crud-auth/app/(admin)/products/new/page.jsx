aacff3e4f498704315e6faab9df5fec38088d79c
Victor Villazón <victorzn23@gmail.com>
2024-07-08 20:40:11 -0500

    semana06, dia01, nextjs crud auth

--- semana05/dia03-nextjs-crud-auth/app/(admin)/products/new/page.jsx
+++ semana05/dia03-nextjs-crud-auth/app/(admin)/products/new/page.jsx
@@ -4,17 +4,17 @@ import { useEffect, useState } from "react"
 import { createProduct } from "../services/products"
 import { fetchCategories } from "../services/categories"
 import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
 
 const ProductNewPage = () => {
   const router = useRouter()
 
-  const INITIAL_FORM = {
-    title: '',
-    price: 0,
-    category: ''
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
 
-  const [form, setForm] = useState(INITIAL_FORM)
   const [categories, setCategories] = useState([])
 
   useEffect(() => {
@@ -22,43 +22,38 @@ const ProductNewPage = () => {
       .then(data => setCategories(data))
   }, [])
 
-  const handleChange = (event) => {
-    const { name, value } = event.target
-
-    setForm({ ...form, [name]: value})
-  }
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleNewProduct = async (data) => {
    
+    console.log(data)
+
     // TODO: implementar el envio por POST a el servicio dummyjson.com
 
-    console.log('Saving...')
+    // console.log('Saving...')
 
-    const res = await createProduct(form)
+    // const res = await createProduct(form)
 
-    console.log(res)
+    // console.log(res)
 
-    setForm(INITIAL_FORM)
+    // setForm(INITIAL_FORM)
 
-    router.push('/products')
+    // router.push('/products')
   }
 
   return (
     <section>
-      <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
+      <form onSubmit={handleSubmit(handleNewProduct)} className="max-w-sm mx-auto">
         <h1 className="text-3xl font-bold mb-4">New product</h1>
         <div className="mb-5">
           <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
-          <input type="title" id="title" name="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required
-          onChange={handleChange}
-          value={form.title} />
+          <input type="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder=""
+          {...register('title', { required: true })}
+          />
+          {errors.title && <div className="text-red-500 mt-2">Title is required</div>}
         </div>
         <div className="mb-5">
           <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900">Price</label>
-          <input type="text" id="price" name="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required
-           onChange={handleChange}
-           value={form.price} />
+          <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+           {...register('price', { required: true })} />
         </div>
         <div className="mb-5">
           <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900">Category</label>
@@ -68,12 +63,8 @@ const ProductNewPage = () => {
           value={form.category} /> */}
 
           <select
-            id="category"
-            name="category"
-            onChange={handleChange}
-            value={form.category}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
+            {...register('category', { required: true })} 
           >
             <option value="">Elija una opción</option>
             {categories.map(category => (
@@ -85,7 +76,7 @@ const ProductNewPage = () => {
           {/* <pre>{JSON.stringify(categories, null, 2)}</pre> */}
         </div>
 
-        <pre>{JSON.stringify(form)}</pre>
+
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Save</button>
       </form>
     </section>
