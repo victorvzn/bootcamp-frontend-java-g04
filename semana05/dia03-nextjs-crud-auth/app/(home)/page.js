03190602f4b78fe79beff5bbd4f09a952be42210
Victor Villazón <victorzn23@gmail.com>
2024-07-08 23:12:31 -0500

    semana06, dia01, nextjs crud auth

--- semana05/dia03-nextjs-crud-auth/app/(home)/page.js
+++ semana05/dia03-nextjs-crud-auth/app/(home)/page.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     <section className="bg-white">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
-          Next.js: CRUD + Auth
+          Next.js: CRUD + Auth (Published)
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
           by <a href="https://victorvillazon.com">Victor Villazón</a>
