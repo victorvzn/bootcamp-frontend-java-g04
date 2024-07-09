3698aa8f1bdbe88aeaa91e5df3cb814ecbe8afd7
Victor Villazón <victorzn23@gmail.com>
2024-07-08 21:08:46 -0500

    semana06, dia01, nextjs crud auth

--- semana05/dia03-nextjs-crud-auth/app/(admin)/products/new/page.jsx
+++ semana05/dia03-nextjs-crud-auth/app/(admin)/products/new/page.jsx
@@ -5,6 +5,20 @@ import { createProduct } from "../services/products"
 import { fetchCategories } from "../services/categories"
 import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+// Definir el schema de validación
+
+const schema = z.object({
+  title: z.string()
+    .min(3, 'El nombre es requerido y debe tener mínimo 3 caracteres')
+    .max(30, 'El nombre no debe pasar los 30 caracteres'),
+  price: z.number()
+    .positive('El precio debe ser mayor a cero')
+    .min(1, 'El precio debe ser mínimo cero'),
+  category: z.string().min(1, 'La categoría es requerida')
+})
 
 const ProductNewPage = () => {
   const router = useRouter()
@@ -13,7 +27,9 @@ const ProductNewPage = () => {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
+  } = useForm({
+    resolver: zodResolver(schema)
+  })
 
   const [categories, setCategories] = useState([])
 
@@ -44,14 +60,15 @@ const ProductNewPage = () => {
         <div className="mb-5">
           <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
           <input type="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder=""
-          {...register('title', { required: true })}
+          {...register('title')}
           />
-          {errors.title && <div className="text-red-500 mt-2">Title is required</div>}
+          {errors.title && <div className="text-red-500 mt-2">{errors.title.message}</div>}
         </div>
         <div className="mb-5">
           <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900">Price</label>
-          <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-           {...register('price', { required: true })} />
+          <input type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+           {...register('price', { valueAsNumber: true })} />
+          {errors.price && <div className="text-red-500 mt-2">{errors.price.message}</div>}
         </div>
         <div className="mb-5">
           <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900">Category</label>
@@ -62,7 +79,7 @@ const ProductNewPage = () => {
 
           <select
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            {...register('category', { required: true })} 
+            {...register('category')} 
           >
             <option value="">Elija una opción</option>
             {categories.map(category => (
@@ -71,6 +88,7 @@ const ProductNewPage = () => {
               </option>
             ))}
           </select>
+          {errors.category && <div className="text-red-500 mt-2">{errors.category.message}</div>}
           {/* <pre>{JSON.stringify(categories, null, 2)}</pre> */}
         </div>
 
