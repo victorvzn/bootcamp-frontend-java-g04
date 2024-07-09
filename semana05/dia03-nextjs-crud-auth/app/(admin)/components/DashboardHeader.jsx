207e213af5edf9f0e47baeaac81e48c67fb8af87
Victor Villazón <victorzn23@gmail.com>
2024-07-08 22:59:03 -0500

    semana06, dia01, nextjs crud auth

--- semana05/dia03-nextjs-crud-auth/app/(admin)/components/DashboardHeader.jsx
+++ semana05/dia03-nextjs-crud-auth/app/(admin)/components/DashboardHeader.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
 
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs"
+
 const DashboardHeader = () => {
   return (
     <header className="bg-gray-100 border-gray-200">
@@ -9,25 +11,31 @@ const DashboardHeader = () => {
         </Link>
       
         <nav className="font-medium flex p-4 md:p-0 border border-gray-100 rounded-lg md:space-x-8 md:mt-0 md:border-0">
-          <Link
-            href="/"
-            className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-          >
-            Home
-          </Link>
-          <Link
-            href="/products"
-            className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-          >
-            Products
-          </Link>
+          <SignedIn>
+            <Link
+              href="/"
+              className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+            >
+              Home
+            </Link>
+            <Link
+              href="/products"
+              className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+            >
+              Products
+            </Link>
+          </SignedIn>
 
-          <Link
-            href="/sign-in"
-            className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-          >
-            Sign in
-          </Link>
+          <SignedOut>
+            <Link
+              href="/sign-in"
+              className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+            >
+              Sign in
+            </Link>
+          </SignedOut>
+
+          <UserButton />
         </nav>
       </div>
     </header>
---
+++ semana05/dia03-nextjs-crud-auth/app/(auth)/layout.jsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({children}) => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
